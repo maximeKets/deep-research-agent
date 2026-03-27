709ee74449f11c705464a6a5767cb0b1d0238df8
maxime <maximekets80@gmail.com>
2026-03-27 09:42:38 +0100

    #5 feat(research-manager): add ResearchManager for planning and performing searches

---
+++ research_manager.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from agents import Runner
+from planner_agent import WebSearchPlan, planner_agent
+
+
+class ResearchManager:
+
+    async def plan_searches(self, query: str) -> WebSearchPlan:
+        """ Plan the searches to perform for the query """
+        print("Planning searches...")
+        result = await Runner.run(
+            planner_agent,
+            f"Query: {query}",
+        )
+        print(f"Will perform {len(result.final_output.searches)} searches")
+        return result.final_output_as(WebSearchPlan)
+
+    async def perform_searches(self, search_plan: WebSearchPlan) -> list[str]:
+        """ Perform the searches to perform for the query """
+        print("Searching...")
+        num_completed = 0
+        tasks = [asyncio.create_task(self.search(item)) for item in search_plan.searches]
+        results = []
+        for task in asyncio.as_completed(tasks):
+            result = await task
+            if result is not None:
+                results.append(result)
+            num_completed += 1
+            print(f"Searching... {num_completed}/{len(tasks)} completed")
+        print("Finished searching")
+        return results
