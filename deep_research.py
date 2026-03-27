edde39fec20d77b8c843075e5f3e17a587ef3e5b
maxime <maximekets80@gmail.com>
2026-03-27 09:49:55 +0100

    #6 feat(research-manager): add script to execute research queries asynchronously

---
+++ deep_research.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from research_manager import ResearchManager
+
+load_dotenv(override=True)
+
+
+async def run(query: str):
+    async for chunk in ResearchManager().run(query):
+        yield chunk
+
